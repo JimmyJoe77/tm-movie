c5ebd47160ce1c4903b394091d87572b1da814e2
JimmyJoe77 <73wxjei8@tempmailpro.io>
2026-04-07 14:02:31 +0700

    Update slug in getUrlList

--- plugins/tmdb_plugin.js
+++ plugins/tmdb_plugin.js
@@ -60,25 +60,25 @@ function getHomeSections() {
 
 function getPrimaryCategories() {
   return JSON.stringify([
-    { id: '28', name: 'Action' },
-    { id: '12', name: 'Adventure' },
-    { id: '16', name: 'Animation' },
-    { id: '35', name: 'Comedy' },
-    { id: '80', name: 'Crime' },
-    { id: '99', name: 'Documentary' },
-    { id: '18', name: 'Drama' },
-    { id: '10751', name: 'Family' },
-    { id: '14', name: 'Fantasy' },
-    { id: '36', name: 'History' },
-    { id: '27', name: 'Horror' },
-    { id: '10402', name: 'Music' },
-    { id: '9648', name: 'Mystery' },
-    { id: '10749', name: 'Romance' },
-    { id: '878', name: 'Science Fiction' },
-    { id: '10770', name: 'TV Movie' },
-    { id: '53', name: 'Thriller' },
-    { id: '10752', name: 'War' },
-    { id: '37', name: 'Western' }
+    { id: '28', slug: 'Action' },
+    { id: '12', slug: 'Adventure' },
+    { id: '16', slug: 'Animation' },
+    { id: '35', slug: 'Comedy' },
+    { id: '80', slug: 'Crime' },
+    { id: '99', slug: 'Documentary' },
+    { id: '18', slug: 'Drama' },
+    { id: '10751', slug: 'Family' },
+    { id: '14', slug: 'Fantasy' },
+    { id: '36', slug: 'History' },
+    { id: '27', slug: 'Horror' },
+    { id: '10402', slug: 'Music' },
+    { id: '9648', slug: 'Mystery' },
+    { id: '10749', slug: 'Romance' },
+    { id: '878', slug: 'Science Fiction' },
+    { id: '10770', slug: 'TV Movie' },
+    { id: '53', slug: 'Thriller' },
+    { id: '10752', slug: 'War' },
+    { id: '37', slug: 'Western' }
   ]);
 }
 
@@ -127,16 +127,10 @@ function getUrlList(slug, filtersJson) {
   try {
     var filters = JSON.parse(filtersJson || "{}");
     var page = filters.page || 1;
-    var language = filters.language || "en-US";
-    var sortBy = filters.sort || "popularity.desc";
     
-    var url = "https://api.themoviedb.org/3/discover/movie";
+    var url = "https://api.themoviedb.org/3/" + slug;
     url += "?api_key=" + TMDB_API_KEY;
     url += "&page=" + page;
-    url += "&language=" + language;
-    url += "&sort_by=" + sortBy;
-    url += "&include_adult=false";
-    url += "&include_video=false";
     
     // Apply genre filter if slug is a genre ID
     if (slug && slug.match(/^\d+$/)) {
